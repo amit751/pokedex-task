16d01fcaa6633e1a95bf359920a5c5d11c9b11ad
amit <amitoosh.mor@gmail.com>
2021-03-18 21:47:27 +0200

    backend-endpoints-requirements

--- server/app.js
+++ server/app.js
@@ -1,13 +1,87 @@
 const express = require("express");
 const cors = require("cors");
-const api = require("./routes");
+const axios = require("axios")
 const app = express();
-
+const POKE_URL="https://pokeapi.co/api/v2/"
+let collection = [];
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static("build"));
+app.get("/api/pokemon/:name" , (req ,res)=>{
+    const name = req.params.name;
+    axios.get(POKE_URL+`pokemon/${name}`)
+    .then(({data})=>{
+        const {name , height , weight , types , sprites} = data;
+        const pokemon = {
+            name:name,
+            height:height ,
+            weight:weight ,
+            types: types ,
+            sprites: {
+                front:sprites["front_default"] ,
+                back:sprites["back_default"]
+            }
+        };
+        console.log(pokemon);
+       return res.json(pokemon);
+    }).catch((err)=>{
+        return res.send(err);
+        console.log(err)
+    });
+});
 
-app.use("/api", api);
+app.get("/api/type/:name", (req, res) => {
+    const name = req.params.name;
+    axios.get(POKE_URL+`type/${name}`)
+    .then(({data}) => {
+        console.log(data);
+        return res.json(data.pokemon);
+    }).catch((err)=>{
+        console.log(err);
+        return res.send(err);
+    })
+});
+
+app.get("/api/collection", (req,res) => {
+    return res.json(collection);
+});
+
+app.post("/api/collection/catch", (req, res) => {
+    const pokemon = req.body;
+    collection.push(pokemon);
+    console.log(collection);
+    res.send("Added to collection");
+
+     
+});
+
+app.delete("/api/collection/release/:name" , (req , res)=>{
+    pokemonName = req.params.name;
+    collection = collection.filter((pokemon)=>{
+        return pokemon.name !== pokemonName;
+    })
+    res.send("Pokemon deleted!")
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 module.exports = app;
+
+
+
+// Should have a POST endpoint /api/collection/catch to add a new pokémon to your collection
+// Should have a DELETE endpoint /api/collection/release/:id to remove a pokémon from your collection
