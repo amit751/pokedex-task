475c34db17522937873be00eb9a85692c50ba464
Maorshl <56290456+Maorshl@users.noreply.github.com>
2021-03-20 01:42:43 +0200

    heroku deploy try 4

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const app = require("./server/app");
 const PORT = process.env.PORT || 3001;
+const proxy = require("http-proxy-middleware");
+
+app.use(proxy(["/api"], { target: "http://localhost:3001" }));
 
 app.listen(PORT, () => {
   console.log(`app listening on ${PORT}`);
