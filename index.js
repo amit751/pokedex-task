c564371d82463c0b8050ed4402c29fbd50149e63
Maorshl <56290456+Maorshl@users.noreply.github.com>
2021-03-20 01:51:37 +0200

    heroku deploy try 6

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 const app = require("./server/app");
 const PORT = process.env.PORT || 3001;
-const proxy = require("http-proxy-middleware");
-
-app.use(proxy(["/api"], { target: "https://pokedex-amit-maor.herokuapp.com" }));
 
 app.listen(PORT, () => {
   console.log(`app listening on ${PORT}`);
